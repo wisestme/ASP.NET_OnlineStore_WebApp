c5556308ccca10bdb7fd768c34121e5a351f94f5
wisestMe <wyhzest@gmail.com>
2020-09-25 17:55:28 +0100

    Add project files.

---
+++ FashionStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FashionStore.Startup))]
+namespace FashionStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
