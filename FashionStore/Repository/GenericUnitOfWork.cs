2d8ac38c27e5f97e22ad081197b56c79754ccbae
wisestMe <wyhzest@gmail.com>
2020-10-11 11:59:06 +0100

    add repository

---
+++ FashionStore/Repository/GenericUnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using FashionStore.AllTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FashionStore.Repository
+{
+    public class GenericUnitOfWork:IDisposable
+    {
+        private CreamchicEntities DBEntity = new CreamchicEntities();
+        public IRepository<Tbl_EntityType> GetRepositoryInstance<Tbl_EntityType>() where Tbl_EntityType : class
+        {
+            return new GenericRepository<Tbl_EntityType>(DBEntity);
+        }
+
+        public void SaveChanges()
+        {
+            DBEntity.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!this.disposed)
+            {
+                if(disposing)
+                {
+                    DBEntity.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool disposed = false;
+
+    }
+}
